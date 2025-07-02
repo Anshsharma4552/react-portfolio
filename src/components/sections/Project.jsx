dc880aa6c53e89856f68137ed7e9eb1dfc4b9534
Ansh Sharma <anshsharma2463@gmail.com>
2025-07-02 11:02:10 +0530

    update

--- src/components/sections/Project.jsx
+++ src/components/sections/Project.jsx
@@ -117,6 +117,20 @@ export const Projects = () => {
                             <a href="https://book-slider-livid.vercel.app/"  target="_blank" className="text-blue-400 hover:text-blue-300 transition-colors my-4">View Project →</a>
                         </div>
                     </div>
+                    <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+                        <h3 className="text-xl font-bold mb-2">Intership-Project</h3>
+                        <p className="text-gray-400 mb-4">Developed a responsive web application using React, Tailwind CSS, React Router DOM, and Framer Motion. Focused on building smooth navigation, animated components, and clean UI layouts. Gained practical experience in modern frontend development and project structuring.</p>
+                        <div>
+                            {["React","Tailwind Css","Framer Motion","React-Router"].map((tech)=>{
+                                return <span key={tech} className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all">
+                                    {tech}
+                                </span>
+                            })}
+                        </div>
+                        <div className="flex justify-between items-center">
+                            <a href="https://internship-project-tan.vercel.app/" target="_blank" className="text-blue-400 hover:text-blue-300 transition-colors my-4">View Project →</a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </RevealOnScroll>
